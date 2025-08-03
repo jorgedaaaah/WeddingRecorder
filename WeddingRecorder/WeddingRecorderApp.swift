b88d4789d9a3e79bf30e20e56592dd8acaf7e32b
Jorge <jor.torresg@gmail.com>
2025-08-02 20:07:28 -0700

    Initial commit of Wedding Recorder. Initial Xcode project for iOS app.

---
+++ WeddingRecorder/WeddingRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeddingRecorderApp.swift
+//  WeddingRecorder
+//
+//  Created by Jorge on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct WeddingRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeddingRecorderTests/WeddingRecorderTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeddingRecorderTests.swift
+//  WeddingRecorderTests
+//
+//  Created by Jorge on 8/2/25.
+//
+
+import Testing
+@testable import WeddingRecorder
+
+struct WeddingRecorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
